3b8e2c45d9abc743fd2f6c2309a7e2ede39113ec
roshan-creator <roshanpatidar161@gmail.com>
2021-09-26 18:48:39 +0530

    Netflix Clone

---
+++ Nav.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://fontmeme.com/images/netflix-new-logo.png"
+          alt="Netflix Logo"
+        />
+
+        <img
+          className="nav__avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          alt="Netflix Logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
